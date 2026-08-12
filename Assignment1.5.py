6ae1a8a002c56858d801720531b34f51057942f8
prasadbavke180 <prasadbavake@gmail.com>
2026-08-12 13:45:16 +0530

    Delete Assignment1.5.py

--- Assignment1.5.py
+++
@@ -1,8 +0,0 @@
-
-
-P = int(input("enter amount of principal:"))
-R = int(input("enter rate of interest:"))
-T = int(input("enter time (years):"))
-
-CI = P*(1 +R/100)**T-P
-print(f"Compound interes is :{CI}")
