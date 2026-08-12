9bbc1e89408c02774689702488cad853d23b48b4
prasadbavke180 <prasadbavake@gmail.com>
2026-08-12 13:44:45 +0530

    Delete Assignment1.3.py

--- Assignment1.3.py
+++
@@ -1,9 +0,0 @@
-# program to find quotient and remainder of two numbers
-
-Divident=(input('Enter the value of Divident:'))
-divisior=(input('Enter the value of divisior:'))
-Q=int(Divident)//int(divisior)
-R=int(Divident)%int(divisior)
-
-print(f"Quotient is:{Q}")
-print(f"Remainder is:{R}")
