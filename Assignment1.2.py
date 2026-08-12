3548615d9e5abed381e3d6ba1c625b8b1eb788fb
prasadbavke180 <prasadbavake@gmail.com>
2026-08-12 13:44:27 +0530

    Delete Assignment1.2.py

--- Assignment1.2.py
+++
@@ -1,8 +0,0 @@
-# Area of reactangle
-
-length=int(input("Enter length of Reactangle"))
-breadth=int(input("Enter breadth of Reactangle"))
-
-area= length * breadth
-
-print(f"Area of Reactangle is{area}")
